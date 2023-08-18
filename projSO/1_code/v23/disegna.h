220059e9c2c1d725d3b7119f2d4847b81b7e64ee
Alex <140197774+alex88Karalis@users.noreply.github.com>
2023-08-18 13:38:12 +0200

    v23 con collisioni rana e waitpid

---
+++ projSO/1_code/v23/disegna.h
@@ -0,0 +1,60 @@
+#ifndef DISEGNA_H
+#define DISEGNA_H
+#include "tronco.h"
+#include "rana.h"
+#include "auto.h"
+#include "logo.h"
+#include "menuIniziale.h"
+#include "inizializza.h"
+#include "proiettile.h"
+#include "nemico.h"
+#include "proiettileNemico.h"
+
+#define OLDPOSDIM 12
+#define MAXNPROIETTILI 10
+#define MAXNNEMICI 3
+#define MAXNPROIETTILINEMICI 10
+
+typedef enum 
+{
+	RANA_SPRITE, TRONCO_SPRITE, AUTO_SPRITE, CAMION_SPRITE, PROIETTILE_SPRITE, PROIETTILE_NEMICO_SPRITE, NEMICO_SPRITE
+}TipoSprite;
+
+#define RANA_ROWS 2
+#define RANA_COLS 3
+
+#define TRONCO_ROWS 2
+#define TRONCO_COLS 9
+
+#define AUTO_ROWS 2
+#define AUTO_COLS 4
+
+#define CAMION_ROWS 2
+#define CAMION_COLS 7
+
+#define PROIETTILE_ROWS 2
+#define PROIETTILE_COLS 1
+
+
+
+typedef struct{
+	int max_row;
+	int max_col;
+	char **sprite;
+	int color;
+}Sprite;
+int id_disponibile(pid_t *array_pid_proiettili,int lunghezza);
+void avviaDrawProcess(int pipe_fd[2]);
+void drawProcess(int* pipe_fd);
+void stampaMatrice( ScreenCell (*screenMatrix)[WIDTH]);
+void aggiornaOldPos(PipeData *old_pos,PipeData *pipeData);
+
+void aggiornaPosizioneOggetto(PipeData *pipeData, PipeData *old_pos, ScreenCell (*screenMatrix)[WIDTH], ScreenCell (*staticScreenMatrix)[WIDTH], Sprite *sprite);
+void stampaSpriteInMatrice(int startRow, int startCol, Sprite* sprite, ScreenCell (*screenMatrix)[WIDTH]);
+void pulisciSpriteInMatrice(int row, int col, Sprite* sprite, ScreenCell (*screenMatrix)[WIDTH], ScreenCell (*staticScreenMatrix)[WIDTH]);
+
+bool collisioneRana( PipeData *old_pos, Sprite *array_sprite);
+bool checkCollisione(PipeData *object_1, PipeData *object_2, Sprite* sprite_1, Sprite* sprite_2);
+bool isBetween (int value, int min_value, int max_value);
+
+#endif
